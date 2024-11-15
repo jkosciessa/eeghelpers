67421f15a4ffd6a470d33f99a9fcddb0b943c216
Julian Kosciessa <julian.kosciessa@donders.ru.nl>
2024-11-15 14:59:12 +0100

    [new] outsource art detection (trial) for flexible cfg call

---
+++ automatic_artifact_correction_trials.m
@@ -0,0 +1,55 @@
+function [data index] = automatic_artifact_correction_trials(cfg, data)
+
+    % requires
+    % cfg.criterion
+    % cfg.recursive
+
+%%  create index for trials to keep
+    index = 1:length(data.trial);
+
+%%  repeat exclusion of trials
+    check = 0;
+    while check == 0;
+
+%%  get artifact contaminated epoch by kurtosis, low & high frequency artifacts
+
+        [indexA parmA zvalA] = cm_MWB_channel_x_epoch_artifacts(cfg,data);
+
+%%  get artifact contaminated epochs by FASTER
+
+        [indexB parmB zvalB] = THG_FASTER_2_epoch_artifacts(cfg,data);
+
+%%  delete trials
+
+        % collect bad trials
+        badtrl = unique([indexA.t; indexB]);
+
+        if prod(size(badtrl)) ~= 0
+            
+            % define trials to keep
+            trials          = 1:length(data.trial);
+            trials(badtrl)  = [];
+
+            % config for deleting trials
+            tmpcfg.trials   = trials;
+            tmpcfg.channel = 'all';
+
+            % update index
+            index(badtrl)   = [];
+
+            % remove trials
+            data = ft_preprocessing(tmpcfg,data);
+            
+            % clear variables
+            clear *A *B badtrl trials tmpcfg labels
+
+        else
+
+            check = 1;
+
+            % clear variables
+            clear *A *B
+            
+        end
+
+    end
